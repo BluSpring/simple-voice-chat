b03dcaa7de7ba5f77b12560aedf3a4a7951f4479
Naz <bluspringindohq@gmail.com>
2023-05-21 03:04:54 +0800

    Fixed 1 memory leak. i think.

--- common_client/src/main/java/de/maxhenkel/voicechat/util/TextureHelper.java
+++ common_client/src/main/java/de/maxhenkel/voicechat/util/TextureHelper.java
@@ -16,29 +16,43 @@ public class TextureHelper {
     public static final float FONT_HEIGHT = 7.99F;
 
     private static final Map<String, BufferedImage> imageMap = new ConcurrentHashMap<>();
+    private static final Map<String, Integer> imageToIdMap = new ConcurrentHashMap<>();
 
     public static String format(String namespace, String path) {
         return String.format("/assets/%s/%s", namespace, path);
     }
 
+    public static void refreshTextures() {
+        imageMap.clear();
+        imageToIdMap.clear();
+    }
+
     public static void bindTexture(String path) {
-        BufferedImage image;
+        if (!imageToIdMap.containsKey(path)) {
+            BufferedImage image;
 
-        if (!imageMap.containsKey(path)) {
-            try {
-                InputStream stream = Voicechat.class.getResourceAsStream(path);
-                image = ImageIO.read(stream);
-                stream.close();
+            if (!imageMap.containsKey(path)) {
+                try {
+                    InputStream stream = Voicechat.class.getResourceAsStream(path);
+                    image = ImageIO.read(stream);
+                    stream.close();
+                    image.flush();
 
-                imageMap.put(path, image);
-            } catch (IOException e) {
-                throw new IllegalStateException(e);
+                    imageMap.put(path, image);
+                } catch (IOException e) {
+                    throw new IllegalStateException(e);
+                }
+            } else {
+                image = imageMap.get(path);
             }
-        } else {
-            image = imageMap.get(path);
-        }
 
-        int id = mc.renderEngine.allocateAndSetupTexture(image);
-        mc.renderEngine.bindTexture(id);
+            int id = mc.renderEngine.allocateAndSetupTexture(image);
+
+            imageToIdMap.put(path, id);
+            mc.renderEngine.bindTexture(id);
+        } else {
+            int id = imageToIdMap.get(path);
+            mc.renderEngine.bindTexture(id);
+        }
     }
 }
---
+++ fabric_client/src/main/java/de/maxhenkel/voicechat/mixin/RenderEngineMixin.java
@@ -0,0 +1,16 @@
+package de.maxhenkel.voicechat.mixin;
+
+import de.maxhenkel.voicechat.util.TextureHelper;
+import net.minecraft.src.RenderEngine;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderEngine.class)
+public class RenderEngineMixin {
+    @Inject(method = "refreshTextures", at = @At("HEAD"))
+    public void resetTextureHelper(CallbackInfo ci) {
+        TextureHelper.refreshTextures();
+    }
+}
