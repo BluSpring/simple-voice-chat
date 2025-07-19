6d2f54f0659602d313d7595a4e0fec9304907787
Naz <bluspringindohq@gmail.com>
2025-07-20 04:27:12 +0800

    Fix stereo, pt 2

--- common_client/src/main/java/de/maxhenkel/voicechat/voice/client/PositionalAudioUtils.java
+++ common_client/src/main/java/de/maxhenkel/voicechat/voice/client/PositionalAudioUtils.java
@@ -53,7 +53,7 @@ public class PositionalAudioUtils {
      * @return a float array of length 2, containing the left and right volume (0-1)
      */
     private static float[] getStereoVolume(Vec3D soundPos) {
-        return getStereoVolume(getCameraPosition(), mc.thePlayer != null ? -mc.thePlayer.rotationYaw : 0F, soundPos);
+        return getStereoVolume(getCameraPosition(), mc.thePlayer != null ? mc.thePlayer.rotationYaw : 0F, soundPos);
     }
 
     /**
